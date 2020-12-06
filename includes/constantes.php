5f80d93a0df01fef337ff54cd424ee298d61d2a6
neo <neorepo.edu@gmail.com>
2020-12-06 09:55:24 -0300

    Agregando caracter & a la constante PASSWORD_SYMBOLS

--- includes/constantes.php
+++ includes/constantes.php
@@ -8,7 +8,7 @@ define('LONGITUD_MINIMA_EMAIL', 6);
 define('LONGITUD_MAXIMA_EMAIL', 30);
 define('MAX_PASSWORD_LENGTH', 60);
 define('MIN_PASSWORD_LENGTH', 8);
-define('PASSWORD_SYMBOLS', '@.-_~!#$%^*');
+define('PASSWORD_SYMBOLS', '@.-_~!#$%^&*');
 // https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#peppering
 define('PEPPER', 'r8UN#uHVX5x~&4+ZaG&y'); // 20 caracteres
 
